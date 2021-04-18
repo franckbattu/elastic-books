f6000b08f41a5eb15094c315839d5423c7dd2070
Franck Battu <franck.battu@outlook.fr>
2021-04-18 18:03:02 +0200

    refactor: clean seeder

--- seeder/main.js
+++ seeder/main.js
@@ -10,22 +10,12 @@ const main = async () => {
 
   const start = async () => {
     await data.forEach(async book => {
+      const { id, ...data } = book;
+
       await client.index({
         index: 'books',
         body: {
-          title: book.title,
-          authors: book.authors,
-          books_count: book.books_count,
-          image: book.image,
-          image_medium: book.image_medium,
-          average_rating: book.average_rating,
-          average_rating_rounded: book.average_rating_rounded,
-          isbn: book.isbn,
-          language_code: book.language_code,
-          original_publication_year: book.original_publication_year,
-          original_series: book.original_series,
-          original_title: book.original_title,
-          ratings_count: book.ratings_count,
+          ...data
         }
       })
     })
